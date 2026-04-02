4cb188ef4e6fc84e1e2098fe13cf2b580f372f15
Tristan Possessky <tlp64@pitt.edu>
2026-04-02 14:17:07 -0400

    updated

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import cv2
 from flask import Flask
-from flask.cli import load_dotenv
-from flask_login import LoginManager
+from flask_session import Session
 
 
 def create_app():
@@ -10,10 +9,14 @@ def create_app():
 
     # Configuration
     app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['PERMANENT_SESSION_LIFETIME'] = 300  # 5 minutes in seconds
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
     # Register blueprints FIRST (before initializing video)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+    Session(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
